1344053386ebedca90573a42a5e8e0d0712520c6
Anisha <anisha@gmail.com>
2017-04-06 07:48:32 -0500

    Added Lightbox feature

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("galPhoto",["ngRoute","ngResource"]);
+var app = angular.module("galPhoto",["ngRoute","ngResource",'ui.bootstrap','bootstrapLightbox']);
 
 app.config(['$routeProvider',function($routeProvider){
        $routeProvider     
--- public/controllers/gallery.controller.js
+++ public/controllers/gallery.controller.js
@@ -1,9 +1,20 @@
 angular.module('galPhoto')
-.controller('GalleryCtrl',['$scope','instagram',function($scope,instagram){
+.controller('GalleryCtrl',['$scope','$uibModal','instagram','Lightbox',function($scope,$uibModal,instagram,Lightbox){
                            
     $scope.names = [];
+    var imgArray = [];
+    
     instagram.fetchPopular(function(data){
         $scope.names=data;
-        console.log(data);
+        
+        //console.log(data);
+        angular.forEach(data,function(value){
+            imgArray.push(value.profile_picture);
+        });
+        
+        $scope.openLightboxModal = function(index){
+            Lightbox.openModal(imgArray,index);
+           // console.log("working");
+        }
     })
 }])
